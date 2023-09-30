ff3c554308a0a4609111e7edc0faccff77bb661f
Ayush <ayushk128@gmail.com>
2023-09-30 17:44:00 +0530

    Added the concept of Hooks, implemented the feature of search top restaurants

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,28 @@
 import RestaurantCard from "./RestaurantCard";
+import { useState } from "react";
 import resList from "../utils/mockData";
 
 // not using keys (not acceptable) <<<< index as key <<<<<<<<<<< unique id(best practice)
 const Body = () => {
+  const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filteredList = listOfRestaurants.filter(
+              (res) => res.info.avgRating > 4.1
+            );
+            setListOfRestaurants(filteredList);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant, index) => (
+        {listOfRestaurants.map((restaurant) => (
           <RestaurantCard key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
