67633d756331651227e720bbc4379fcf1c98f5d0
Ayush <ayushk128@gmail.com>
2023-10-16 17:13:52 +0530

    Added fetch function to fetch swiggy api

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,10 +1,23 @@
 import RestaurantCard from "./RestaurantCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import resList from "../utils/mockData";
 
 // not using keys (not acceptable) <<<< index as key <<<<<<<<<<< unique id(best practice)
 const Body = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9095043&lng=77.6666806&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
+
+    const json = await data.json();
+    console.log(json);
+  };
 
   return (
     <div className="body">
