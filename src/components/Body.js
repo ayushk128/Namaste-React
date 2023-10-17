b825178bd8f7a45528ba18727a6002ef26c23c5d
Ayush <ayushk128@gmail.com>
2023-10-17 17:43:24 +0530

    Added Loading feature while the component renders

--- src/components/Body.js
+++ src/components/Body.js
@@ -10,14 +10,21 @@ const Body = () => {
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=24.2068407&lng=86.265377&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.8991793&lng=77.6669909&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
 
     const json = await data.json();
 
     console.log(json);
-    setListOfRestaurants(json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
-  }
+    setListOfRestaurants(
+      json?.data?.cards[2]?.card?.card?.gridElements?.infoWithStyle?.restaurants
+    );
+  };
 
+  if(listOfRestaurants.length === 0) {
+    return <h1>Loading.....</h1>
+  }
   return (
     <div className="body">
       <div className="filter">
